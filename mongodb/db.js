f9fba55e1d0af950faeeb6e7d18c711939f9c785
moridodos <coderdhk06@gmail.com>
2025-05-24 12:25:41 +0900

    add mongodb

---
+++ mongodb/db.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+export async function connectDB() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/testdb');
+  console.log('MongoDB connected');
+}
---
+++ mongodb/index.js
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import { connectDB } from './db.js';
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  email: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+await connectDB();
+
+// CREATE
+await User.create({ name: '철수', age: 20, email: 'chulsoo@example.com' });
+
+// READ
+const users = await User.find();
+console.log('📋 Users:', users);
+
+// UPDATE
+await User.updateOne({ name: '철수' }, { age: 21 });
+
+// DELETE
+// await User.deleteOne({ name: '철수' });
+
+process.exit();
